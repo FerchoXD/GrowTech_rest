72d8b0a52387e74e77ddc6facaa7524768111a41
FerchoKD <213524@ids.upchiapas.edu.mx>
2023-07-14 12:42:45 -0600

    Funcion validar usuario-planta

--- apps/plants/api/urls.py
+++ apps/plants/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from apps.plants.views import PlantaList, PlantaCreate,PlantaRetrieveUpdateDestroy
+from apps.plants.views import PlantaList, PlantaCreate,PlantaRetrieveUpdateDestroy,ValidacionPlants
 
 urlpatterns = [
     path('', PlantaList.as_view(), name='plant-list'),
     path('create/', PlantaCreate.as_view(), name='plant-create'),
     path('<int:pk>/', PlantaRetrieveUpdateDestroy.as_view(), name='plant-retrieve-update-destroy'),
+    path('validar/planta/',ValidacionPlants.as_view(),name= 'planta-validacion')
 ]
--- apps/plants/views.py
+++ apps/plants/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
-from rest_framework.generics import ListAPIView,CreateAPIView,RetrieveUpdateDestroyAPIView
+from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.permissions import IsAuthenticated
-from apps.plants.models import Planta
+from apps.plants.models import Planta,User
 from .api.serializers import PlantaSerializer
-
+from rest_framework.generics import GenericAPIView
+from rest_framework import status
+from rest_framework.response import Response
+from django.core.exceptions import ObjectDoesNotExist
 class PlantaList(ListAPIView):
     queryset = Planta.objects.all()
     serializer_class = PlantaSerializer
@@ -18,3 +21,32 @@ class PlantaRetrieveUpdateDestroy(RetrieveUpdateDestroyAPIView):
     queryset = Planta.objects.all()
     serializer_class = PlantaSerializer
     permission_classes = [IsAuthenticated]
+
+
+class ValidacionPlants(GenericAPIView):
+    def post(self, request, *args, **kwargs):
+        data = request.data
+
+        try:
+            nombre_planta = data['nombre']
+            nombre_usuario = data['nombreU']
+
+            # Verificar si existen los campos en la base de datos
+            planta = Planta.objects.get(nombre=nombre_planta)
+            usuario = User.objects.get(username=nombre_usuario)
+
+            response_data = {
+                'response': 'Los datos existen en la base de datos',
+                'id_usuario': usuario.id,
+                'id_planta': planta.id,
+                'nombre_planta': nombre_planta,
+                'nombre_usuario': nombre_usuario,
+            }
+            return Response(response_data, status=status.HTTP_200_OK)
+
+        except ObjectDoesNotExist:
+            # Al menos uno de los campos no existe en la base de datos
+            response_data = {
+                'response': 'Los datos no existen en la base de datos',
+            }
+            return Response(response_data, status=status.HTTP_404_NOT_FOUND)
