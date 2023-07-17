66d3cea18967741f18a59306831c058627ea3f41
FerchoKD <213524@ids.upchiapas.edu.mx>
2023-07-17 11:15:42 -0600

    Get temperatura-ultimo-dato

--- apps/ST/api/urls.py
+++ apps/ST/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from apps.ST.views import TemperaturaListCreateAPIView, TemperaturaRetrieveUpdateDestroyAPIView
+from apps.ST.views import TemperaturaListCreateAPIView, TemperaturaRetrieveUpdateDestroyAPIView,UltimoDatoTemperaturaAPIView
 
 urlpatterns = [
     path('',TemperaturaListCreateAPIView.as_view(),name='temperatura-list-create'),
     path('<int:pk>/',TemperaturaRetrieveUpdateDestroyAPIView.as_view(),name='temperatura-detail'),
+    path('temperaturas/ultimo-dato/', UltimoDatoTemperaturaAPIView.as_view(), name='ultimo-dato-temperatura'),
 ]
--- apps/ST/views.py
+++ apps/ST/views.py
@@ -1,7 +1,8 @@
 from rest_framework import generics
 from .models import Temperatura
 from .api.serializers import TemperaturaSerializer
-
+from .models import Temperatura
+from .api.serializers import TemperaturaSerializer
 class TemperaturaListCreateAPIView(generics.ListCreateAPIView):
     queryset = Temperatura.objects.all()
     serializer_class = TemperaturaSerializer
@@ -9,3 +10,7 @@ class TemperaturaListCreateAPIView(generics.ListCreateAPIView):
 class TemperaturaRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Temperatura.objects.all()
     serializer_class = TemperaturaSerializer 
+
+class UltimoDatoTemperaturaAPIView(generics.ListAPIView):
+    queryset = Temperatura.objects.order_by('-fecha_hora')[:1]
+    serializer_class = TemperaturaSerializer
